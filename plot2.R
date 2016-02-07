824bee71c1dbf5094e15ef42c7ea182d2656f1f0
ntvcuong <cuongvietnguyenthanh@gmail.com>
2016-02-08 00:49:59 +0700

    Project

--- plot2.R
+++ plot2.R
@@ -4,6 +4,9 @@ data <- read.table("./household_power_consumption.txt", sep = ';', header = TRUE
 data <- data[data$Date == '1/2/2007' | data$Date == '2/2/2007',]
 data$datetime <- dmy_hms(paste(data$Date,data$Time))
 data$Global_active_power <- as.numeric(levels(data$Global_active_power))[data$Global_active_power]
+data$Sub_metering_1 <- as.numeric(levels(data$Sub_metering_1))[data$Sub_metering_1]
+data$Sub_metering_2 <- as.numeric(levels(data$Sub_metering_2))[data$Sub_metering_2]
+data$Sub_metering_3 <- as.numeric(levels(data$Sub_metering_3))[data$Sub_metering_3]
 data$Voltage <- as.numeric(levels(data$Voltage))[data$Voltage]
 data$Global_reactive_power <- as.numeric(levels(data$Global_reactive_power))[data$Global_reactive_power]
 #Construct the plot
